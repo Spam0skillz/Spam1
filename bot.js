597c2766bc16eec1761737e34553c409ba772f8f
Spam0skillz <45464812+Spam0skillz@users.noreply.github.com>
2018-11-30 13:31:35 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -72,7 +72,8 @@ if (message.content === '!spam') {
       let count = 0;
       let ecount = 0;
       for(let x = 0; x < 90000; x++) {
-        message.channel.send(`**Credit Spam , Frame , Credit By Kahrba , Farm Credit By Kahrba, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
+        message.channel.send(`**Credit Spam , Frame , Credit By Skillz , Farm Credit By Kahrba, ez Farm - MrCrazy Asoouma Skillz
+**[ " ${x} " ]`)
           .then(m => {
             count++;
           })
